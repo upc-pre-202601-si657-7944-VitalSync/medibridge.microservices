2f4bf46f46e35986d26b602d615ac2f56b4fdfbe
psfa29 <parodrez29@gmail.com>
2026-06-07 17:50:33 -0500

    feat: add docker compose setup

---
+++ docker/postgres-init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE medibridge_profiles;
+CREATE DATABASE medibridge_payments;
+CREATE DATABASE medibridge_appointments;
+CREATE DATABASE medibridge_healthmonitoring;
+CREATE DATABASE medibridge_medication;
+CREATE DATABASE medibridge_reports;
